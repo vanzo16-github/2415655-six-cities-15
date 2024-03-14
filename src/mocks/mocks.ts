73eb0dec6868fe066dba18ff0a12469b07d858ea
Ivan Zotov <vanzo16-workmail@ya.ru>
2024-03-14 13:52:08 +0300

    fixed near offers

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -150,4 +150,54 @@ export const cards: TCard[] = [
       },
     ]
   },
+  {
+    id: '4',
+    title: 'Offer-3',
+    type: 'apartment',
+    previewImage: 'img/apartment-01.jpg',
+    price: 100,
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.3809553943508,
+        longitude: 4.939309666406198,
+        zoom: 8
+      }
+    },
+    location: {
+      latitude: 54.35514938496378,
+      longitude: 1.673877537499948,
+      zoom: 8
+    },
+    isFavorite: true,
+    isPremium: true,
+    rating: 2,
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    bedrooms: 3,
+    goods: [
+      'Wi-Fi', 'Washing machine', 'Towels', 'Heating', 'Coffee machine', 'Baby seat', 'Kitchen', 'Dishwasher', 'Cable TV', 'Fridge'
+    ],
+    host: {
+      name: 'Oliver Conner',
+      avatarUrl: 'https://url-to-image/image.png',
+      isPro: false
+    },
+    images: [
+      'img/apartment-01.jpg', 'img/apartment-01.jpg', 'img/apartment-01.jpg'
+    ],
+    maxAdults: 4,
+    comments: [
+      {
+        id: 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+        date: '2019-05-08T14:13:56.569Z',
+        user: {
+          name: 'Oliver Conner',
+          avatarUrl: 'https://url-to-image/image.png',
+          isPro: false
+        },
+        comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+        rating: 2
+      },
+    ]
+  },
 ];
--- src/pages/offer-screen/offer-screen.tsx
+++ src/pages/offer-screen/offer-screen.tsx
@@ -1,11 +1,12 @@
 import Header from '../../components/header/header';
 import OfferContainer from '../../components/offer-container/offer-container';
 import OfferGallery from '../../components/offer-gallery/offer-gallery';
-import NearPlaces from '../../components/near-places/near-places';
+//import NearPlaces from '../../components/near-places/near-places';
 import { TCard } from '../../mocks/types';
 import { useParams } from 'react-router-dom';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 import Map from '../../components/map/map';
+import PlaceCard from '../../components/place-card/place-card';
 
 type OfferScreenProps = {
   cards: TCard[];
@@ -14,13 +15,14 @@ type OfferScreenProps = {
 function OfferScreen({cards}: OfferScreenProps): JSX.Element {
   const { id } = useParams();
   const offerParams = cards.find((item) => item.id === id);
+  const nearOffers = cards.filter((item) => item.id !== id);
+  const nearOffersThree = nearOffers.slice(0, 3);
 
   if (typeof offerParams === 'undefined') {
     return <NotFoundScreen />;
   }
 
   const {images, title, isPremium, maxAdults, type, bedrooms, goods, price, comments} = offerParams;
-
   return (
     <div className="page">
       <Header/>
@@ -29,13 +31,22 @@ function OfferScreen({cards}: OfferScreenProps): JSX.Element {
         <section className="offer">
           <OfferGallery images={images}/>
           <OfferContainer title={title} isPremium={isPremium} maxAdults={maxAdults} type={type} bedrooms={bedrooms} features={goods} price={price} reviews={comments}/>
-          <Map cards={cards} classMap='offer__map'/>
+          <Map cards={nearOffersThree} classMap='offer__map'/>
 
         </section>
         <div className="container">
           <section className="near-places places">
             <h2 className="near-places__title">Other places in the neighborhood</h2>
-            <NearPlaces cards={cards}/>
+            <div className="near-places__list places__list">
+              {nearOffersThree.map((card) =>(
+                <PlaceCard
+                  key={card.id}
+                  card={card} handleHover={function (): void {
+                    throw new Error('Function not implemented.');
+                  } }
+                />
+              ))}
+            </div>
           </section>
         </div>
       </main>
