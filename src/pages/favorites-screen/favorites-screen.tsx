651eedee786c7f31e87b3da23937aad5b05e0447
Ivan Zotov <vanzo16-workmail@ya.ru>
2024-03-29 13:34:48 +0300

    fixed errors lint

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -1,30 +1,30 @@
-import { Helmet } from 'react-helmet-async';
-import FavoritesList from '../../components/favorites-list/favorites-list';
-import Footer from '../../components/footer/footer';
-import Header from '../../components/header/header';
-import { useAppSelector } from '../../hooks';
+// import { Helmet } from 'react-helmet-async';
+// import FavoritesList from '../../components/favorites-list/favorites-list';
+// import Footer from '../../components/footer/footer';
+// import Header from '../../components/header/header';
+// import { useAppSelector } from '../../hooks';
 
-function FavoritesScreen(): JSX.Element {
-  const offers = useAppSelector((state) => state.offers.cards);
-  const favoriteOffers = offers.filter((card) => card.isFavorite);
-  return (
-    <div className="page">
-      <Header/>
+// function FavoritesScreen(): JSX.Element {
+//   const offers = useAppSelector((state) => state.offers.cards);
+//   const favoriteOffers = offers.filter((card) => card.isFavorite);
+//   return (
+//     <div className="page">
+//       <Header/>
 
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <Helmet>
-              <title>Saved listing</title>
-            </Helmet>
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoritesList cards={favoriteOffers}/>
-          </section>
-        </div>
-      </main>
-      <Footer/>
-    </div>
-  );
-}
+//       <main className="page__main page__main--favorites">
+//         <div className="page__favorites-container container">
+//           <section className="favorites">
+//             <Helmet>
+//               <title>Saved listing</title>
+//             </Helmet>
+//             <h1 className="favorites__title">Saved listing</h1>
+//             <FavoritesList cards={favoriteOffers}/>
+//           </section>
+//         </div>
+//       </main>
+//       <Footer/>
+//     </div>
+//   );
+// }
 
-export default FavoritesScreen;
+// export default FavoritesScreen;
